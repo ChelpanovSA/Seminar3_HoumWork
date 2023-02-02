6631b1adae6e952189f05767cb843d361db4b8bd
Сергей Челпанов <S.inter.4@mail.ru>
2023-02-02 16:41:09 +0300

    Задача_1

---
+++ Task_1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет; 12821 -> да; 3432 -> да.
+
+Console.Clear();
+Console.Write("введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int numA = 0;
+int numB = num;
+int result = 0;
+
+while ( numB > 0)
+{
+numA = numB % 10;
+result = result * 10 + numberA;
+numB = numB / 10;
+}
+if ( num == result) Console.WriteLine($"введенное число: {num} => является палиндромом.");
+else
+{
+Console.Write($"введенное число: {num} => НЕ является палиндромом.");
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// *Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет, 12821 -> да, 23432 -> да*
+
+
+
+Console.Clear();
+Console.Write("введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int numA = num / 10000;
+int numB = num / 1000 % 10;
+int numC = num / 10 % 10;
+int numD = num % 10;
+
+if (numA == numD && numB == numC)
+{
+ Console.WriteLine($"введенное число: {num} => является палиндромом.");
+}
+else Console.Write($"введенное число: {num} => НЕ является палиндромом.");
+
